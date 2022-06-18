2ad48a3143bf5940cdb2e3297cc69197909b4dc5
blck-snwmn <whitesnowmancreator@gmail.com>
2022-06-19 00:00:14 +0900

    Show tauri command result value

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,31 @@
 import { useState } from 'react'
 import logo from './logo.svg'
-import './App.css'
 import { invoke } from '@tauri-apps/api'
 
 
-
 function App() {
-  const [count, setCount] = useState(0)
-  invoke('greet', { name: 'World' })
-    // `invoke` returns a Promise
-    .then((response) => console.log(response))
+  const [name, setName] = useState("")
+  const [invokeValue, setInvokeValue] = useState("")
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div>
+      <p>Hello Tauri</p>
+      <p>
+        <input onChange={(e) => { setName(e.target.value) }} />
+        <button type="button" onClick={() => {
+          invoke('greet', { name: name })
+            // `invoke` returns a Promise
+            .then((response) => {
+              console.log(response)
+              if (typeof response == "string") {
+                setInvokeValue(response);
+              }
+            })
+        }}>
+          invoke
+        </button>
+      </p>
+      <div>invoke value:</div>
+      <div>{invokeValue}</div>
     </div>
   )
 }
